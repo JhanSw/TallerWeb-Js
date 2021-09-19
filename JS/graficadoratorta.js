2eebfd6f9d15b4ffe3b726849025e9328259625a
Your Name <jhanrcc@hotmail.com>
2021-09-19 12:23:18 -0500

    final changes

---
+++ JS/graficadoratorta.js
@@ -0,0 +1,44 @@
+var ctx = document.getElementById('Migrafica').getContext('2d');
+
+document.addEventListener('DOMContentLoaded', () => {
+    const boton = document.getElementById('botonmain')
+    boton.addEventListener('click', () => {
+        const labels = document.getElementById('labels')
+        const data = document.getElementById('data')
+
+        const listaLabels = labels.value.split(',')
+        const listaData = data.value.split(',')
+
+        var myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: listaLabels,
+                datasets: [{
+                    label: 'Datos',
+                    data: listaData,
+                    backgroundColor: [
+                        'rgba(21, 67, 96, 0.7)',
+                        'rgba(120, 40, 31, 0.7)',
+                        'rgba(28, 27, 23, 0.5)'
+                        
+                    ],
+                    borderColor: [
+                        'rgba(21, 67, 96, 1)',
+                        'rgba(120, 40, 31, 1)',
+                        'rgba(28, 27, 23, 1)'
+                    ],
+                    borderWidth: 2
+                    
+                }],
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    })
+})
+
